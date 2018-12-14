573a8da05a5c4517aef8f8ff6555450901f4b3c3
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-14 15:30:32 -0200

    remove engine key form package.json

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+let PORT = process.env.PORT || 7000;
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(7001);
+app.listen(PORT);
