7ed5fe619edea68a69820dd825b9a9894c123cdf
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-14 12:09:13 -0200

    a try with static site

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// server.js
-
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 5000, function(){
-    console.log('Your node js server is running');
-});
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 5000);
