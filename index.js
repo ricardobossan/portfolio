b01dd599e7124b8ce71c5efe5a4815e8227d6b04
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-14 15:32:43 -0200

    change path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let PORT = process.env.PORT || 7000;
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/'));
 });
 
 app.listen(PORT);
