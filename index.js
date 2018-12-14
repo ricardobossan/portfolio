b62d8d125f313e0930b163286878f136edd0fc02
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-14 18:00:36 -0200

    attempt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let PORT = process.env.PORT || 7000;
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(7001);
