21b68aad21fb5de0c6179119894ff44a4a322da3
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-15 15:55:24 -0200

    back to index.html, due to 'Error: ENOENT: no such file or directory, stat '/app/index''

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ showTimes = () => {
 =======
 */// viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT);
