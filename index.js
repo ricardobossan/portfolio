f24a692e6cbccf1bfb582a2e0e003ae3be80e7c3
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-15 16:22:08 -0200

    update

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-let PORT = process.env.PORT || 7000;
-
-// viewed at http://localhost:8080
-/*app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index'));
-});
-||||||| merged common ancestors
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-const cool = require('cool-ascii-faces')
+/*const cool = require('cool-ascii-faces')
+*/
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-=======
-var express = require('express');
-var app = express();
-var path = require('path');
-let PORT = process.env.PORT || 7000;
->>>>>>> b62d8d125f313e0930b163286878f136edd0fc02
 
-<<<<<<< HEAD
-app.listen(7001);
-||||||| merged common ancestors
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join(__dirname, '/'))) /*changed from public to //*
+/*  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
-=======
-*/// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.render('index.html');
-});
-
-app.listen(PORT);
+*/
+  .get('/', (req, res) => res.render('index'))
+/*  .get('/cool', (req, res) => res.send(cool()))
+*/
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
