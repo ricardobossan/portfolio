8a5001d316f63222db33d5a77670330f48cdb6a4
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-16 19:37:54 -0200

    update

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, '/'))) /*changed from public to //*
-/*  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-*/
-  .get('/', (req, res) => res.render('index'))
-/*  .get('/cool', (req, res) => res.send(cool()))
-*/
+  .use(express.static(path.join(__dirname, '')))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
