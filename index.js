0ad8356a1a7c6902b01cee8af461f665a87851bf
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-17 00:18:42 -0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-/*const cool = require('cool-ascii-faces')
-*/
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, '/')))
+  .use(express.static(path.join(__dirname, '')))
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
